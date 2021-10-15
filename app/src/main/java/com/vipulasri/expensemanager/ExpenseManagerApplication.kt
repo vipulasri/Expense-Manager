6b4cf2c6063e196f48fa722a7413aea8ac1afaf6
vipulasri <vipulasri.2007@gmail.com>
2021-10-15 22:24:11 +0530

    add: hilt and room library

---
+++ app/src/main/java/com/vipulasri/expensemanager/ExpenseManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.vipulasri.expensemanager
+
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Vipul Asri on 15/10/21.
+ */
+
+@HiltAndroidApp
+class ExpenseManagerApplication {
+}
