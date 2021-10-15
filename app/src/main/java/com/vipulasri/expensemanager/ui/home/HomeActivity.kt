4c221b6b66fb66ced7ccc4967d30e351ec0a3cab
vipulasri <vipulasri.2007@gmail.com>
2021-10-16 00:51:36 +0530

    add: content summary

---
+++ app/src/main/java/com/vipulasri/expensemanager/ui/home/HomeActivity.kt
@@ -0,0 +1,47 @@
+package com.vipulasri.expensemanager.ui.home
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.vipulasri.expensemanager.databinding.ActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    private val viewModel: HomeVM by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.totalIncome.observe(this, { income ->
+            updateAmount(binding.content.textIncome, income)
+        })
+
+        viewModel.totalExpense.observe(this, { expenses ->
+            updateAmount(binding.content.textExpense, expenses)
+        })
+
+        viewModel.balance.observe(this, { balance ->
+            updateAmount(binding.content.textBalance, balance)
+        })
+    }
+
+    private fun updateAmount(textView: TextView, amount: Double?) {
+        textView.text = amount?.let { safeAmount ->
+            "$$safeAmount"
+        }?: "--"
+    }
+
+}
