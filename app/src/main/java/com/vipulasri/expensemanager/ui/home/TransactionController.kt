0920aa90d1bdf594dbe0e390cf8f7f50830a579f
vipulasri <vipulasri.2007@gmail.com>
2021-10-19 10:44:01 +0530

    add: empty transactions state

--- app/src/main/java/com/vipulasri/expensemanager/ui/home/TransactionController.kt
+++ app/src/main/java/com/vipulasri/expensemanager/ui/home/TransactionController.kt
@@ -3,6 +3,7 @@ package com.vipulasri.expensemanager.ui.home
 import com.airbnb.epoxy.TypedEpoxyController
 import com.vipulasri.expensemanager.data.local.entity.TransactionEntity
 import com.vipulasri.expensemanager.data.local.entity.TransactionType
+import com.vipulasri.expensemanager.emptyTransactions
 import com.vipulasri.expensemanager.model.TransactionUiModel
 import com.vipulasri.expensemanager.section
 import com.vipulasri.expensemanager.transaction
@@ -16,12 +17,23 @@ class TransactionController : TypedEpoxyController<List<TransactionUiModel>>() {
     override fun buildModels(data: List<TransactionUiModel>?) {
         data?.takeIf { it.isNotEmpty() }
             ?.forEach { uiModel ->
-                if (uiModel.transactions.isEmpty()) return@forEach
+
+                if (uiModel.transactions.isEmpty()) {
+                    buildEmptyTransaction()
+                    return@forEach
+                }
+
                 buildSection(uiModel.date)
                 uiModel.transactions.forEach { transaction ->
                     buildTransaction(transaction)
                 }
-            }
+            }?: buildEmptyTransaction()
+    }
+
+    private fun buildEmptyTransaction() {
+        emptyTransactions {
+            id("empty-transactions")
+        }
     }
 
     private fun buildSection(title: String) {
